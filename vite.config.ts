8787c3acccffbc7993701815bc074bdbe97b8d35
ljh0608 <m98m@catholic.ac.kr>
2024-01-05 03:21:55 +0900

    style: emotion babel-plugin 설정 - label, sourceMap

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: [
+          [
+            '@emotion/babel-plugin',
+            {
+              autoLabel: 'dev-only',
+              labelFormat: '[filename]__[local]',
+            },
+          ],
+        ],
+      },
+    }),
+  ],
+});
