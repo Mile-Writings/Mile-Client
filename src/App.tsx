1f6504f27f299a7c47022f0f5d31c3850c1bed42
Moon Dahyun <dahyun723@ajou.ac.kr>
2024-01-05 18:19:01 +0900

    fix : reactcomponent 에러

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-// import { Rect } from './assets/svgs';
-import MySvgComponent from './assets/svgs/Rect2';
-// import { ReactComponent as Rect } from '../src/assets/svgs/Rectangle 17.svg';
+import MySvgComponent from './assets/svgs/MySvgComponent.tsx';
+// import { ReactComponent as Rect3 } from '../src/assets/svgs/Rectangle 17.svg';
+import Test3 from '../src/assets/svgs/Rectangle 17.svg?react';
 
 const App = () => {
   return (
     <>
       <h1>마일 웨비 화이팅</h1>
-      {/* <Rect /> */}
+      <Test3 />
       <MySvgComponent />
     </>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import ViteSvgPlugin from 'vite-plugin-svgr';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ViteSvgPlugin()],
+  plugins: [react(), svgr()],
 });
