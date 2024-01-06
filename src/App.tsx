35f2cc93fccca514372ed7940fd34de1878bfec0
Moon Dahyun <dahyun723@ajou.ac.kr>
2024-01-06 19:13:45 +0900

    feat:test

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
+import styled from '@emotion/styled';
+
 import { Rect } from './assets/svgs';
 
 const App = () => {
   //test
   return (
     <>
-      <h1>마일 웨비 화이팅</h1>
+      <StyledH1>마일 웨비 화이팅</StyledH1>
       <Rect />
     </>
   );
 };
 
 export default App;
+
+const StyledH1 = styled.h1`
+  /* Positioning */
+  position: absolute;
+  display: block;
+  top: 0;
+  padding: 10px;
+  right: 0;
+  height: 100px;
+
+  bottom: 0;
+  left: 0;
+  z-index: 10;
+
+  float: right;
+  width: 100px;
+  margin: 10px;
+
+  /* Typography */
+  color: #888;
+  line-height: 1.3;
+  text-align: center;
+`;
