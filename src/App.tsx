95010f1df42213d2d7e333fd27cd1c96a9f82ace
Moon Dahyun <dahyun723@ajou.ac.kr>
2024-01-07 01:17:23 +0900

    fix : .stylelintrc ignore 에러

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
+import styled from '@emotion/styled';
+
 import { Rect } from './assets/svgs';
 
 const App = () => {
   //test
   return (
     <>
-      <h1>마일 웨비 화이팅</h1>
+      <StyledH1>마일 웨비 화이팅</StyledH1>
       <Rect />
     </>
   );
 };
 
 export default App;
+
+const StyledH1 = styled.h1`
+  display: flex;
+  width: 200px;
+  margin: 10px;
+  padding: 10px;
+
+  background-color: black;
+
+  color: #e9ecef;
+  font-weight: bold;
+  font-size: 14px;
+
+  animation: ease;
+  border-radius: 100%;
+`;
