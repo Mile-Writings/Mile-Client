c41608bde295dc567bf9f6bece5f15ffec6d1de6
namdaeun <nde40345@gmail.com>
2024-01-06 14:43:39 +0900

    feat: color 적용되는지 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import styled from '@emotion/styled';
+
+type HeaderProps = {
+  type: boolean;
+};
+
 const App = () => {
   //test
   return (
     <>
-      <h1>마일 웨비 화이팅</h1>
+      <Header type={true}>마일 웨비 화이팅</Header>
     </>
   );
 };
 
 export default App;
+
+const Header = styled.h1<HeaderProps>`
+  font-size: small;
+  color: ${({ theme }) => theme.colors.__main_point_violet};
+`;
