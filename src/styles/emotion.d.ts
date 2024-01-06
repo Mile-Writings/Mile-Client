423db1b9054e7b94d9cd50bd04f28fa45ebe5092
namdaeun <nde40345@gmail.com>
2024-01-06 14:43:02 +0900

    feat: emotion.d.ts 파일 colors 추가

--- src/styles/emotion.d.ts
+++ src/styles/emotion.d.ts
@@ -1,13 +1,27 @@
 import '@emotion/react';
 
-type colors = 'primary' | 'positive' | 'negative';
-
 declare module '@emotion/react' {
   export interface Theme {
     colors: {
-      primary: string;
-      positive: string;
-      negative: string;
+      __main_mile_violet: string;
+      __main_base_violet: string;
+      __main_point_violet: string;
+      __main_mile_green: string;
+      __main_base_green: string;
+      __main_point_green: string;
+      __gray_black: string;
+      __gray_gray_10: string;
+      __gray_gray_9: string;
+      __gray_gray_8: string;
+      __gray_gray_7: string;
+      __gray_gray_6: string;
+      __gray_gray_5: string;
+      __gray_gray_4: string;
+      __gray_gray_3: string;
+      __gray_gray_2: string;
+      __gray_gray_1: string;
+      __gray_gray_05: string;
+      __gray_white: string;
     };
   }
 }
