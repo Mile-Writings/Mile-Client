e3793cd3afed1254a89129fcb8005153fe089d31
namdaeun <nde40345@gmail.com>
2024-01-06 14:40:47 +0900

    feat: theme파일에 colors 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,25 @@
 export const theme = {
   colors: {
-    primary: 'hotpink',
-    positive: 'green',
-    negative: 'red',
+    __main_mile_violet: '#B5B5E6',
+    __main_base_violet: '#EFEFFE',
+    __main_point_violet: '#6139D1',
+    __main_mile_green: '#C5DB8F',
+    __main_base_green: '#FBFFF0',
+    __main_point_green: '#2F7417',
+    __gray_black: '#000',
+    __gray_gray_10: '#1f1f1f',
+    __gray_gray_9: '#333',
+    __gray_gray_8: '#505050',
+    __gray_gray_7: '#747474',
+    __gray_gray_6: '#959595',
+    __gray_gray_5: '#A5A5A5',
+    __gray_gray_4: '#BCBCBC',
+    __gray_gray_3: '#D3D3D3',
+    __gray_gray_2: '#EAEAEA',
+    __gray_gray_1: '#F4F4F4',
+    __gray_gray_05: '#F9F9F9',
+    __gray_white: '#FFF',
   },
 };
+
+export default theme;
