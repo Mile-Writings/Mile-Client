01450462201a26008bd33c8436152026e0796ee5
namdaeun <nde40345@gmail.com>
2024-01-06 15:05:24 +0900

    feat: fonts 객체 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,3 +1,5 @@
+import { css } from '@emotion/react';
+
 export const theme = {
   colors: {
     __main_mile_violet: '#B5B5E6',
@@ -19,6 +21,113 @@ export const theme = {
     __gray_gray_1: '#F4F4F4',
     __gray_gray_05: '#F9F9F9',
     __gray_white: '#FFF',
+  },
+  fonts: {
+    large_title: css`
+      font-family: Pretendard;
+      font-size: 36px;
+      font-style: normal;
+      font-weight: 800;
+      line-height: normal;
+    `,
+    title1: css`
+      font-family: Pretendard;
+      font-size: 30px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: normal;
+    `,
+    title2: css`
+      font-family: Pretendard;
+      font-size: 24px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: normal;
+    `,
+    title3: css`
+      font-family: Pretendard;
+      font-size: 24px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: normal;
+    `,
+    subtitle1: css`
+      font-family: Pretendard;
+      font-size: 20px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: normal;
+    `,
+    subtitle2: css`
+      font-family: Pretendard;
+      font-size: 18px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: normal;
+    `,
+    subtitle3: css`
+      font-family: Pretendard;
+      font-size: 18px;
+      font-style: normal;
+      font-weight: 500;
+      line-height: normal;
+    `,
+    subtitle4: css`
+      font-family: Pretendard;
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: normal;
+    `,
+    subtitle5: css`
+      font-family: Pretendard;
+      font-size: 16px;
+      font-style: normal;
+      font-weight: 500;
+      line-height: normal;
+    `,
+    subtitle6: css`
+      font-family: Pretendard;
+      font-size: 16px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: normal;
+    `,
+    body1: css`
+      font-family: Pretendard;
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: normal;
+    `,
+    body2: css`
+      font-family: Pretendard;
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 500;
+      line-height: normal;
+    `,
+    body3: css`
+      font-family: Pretendard;
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: normal;
+    `,
+    body4: css`
+      font-family: Pretendard;
+      font-size: 12px;
+      font-style: normal;
+      font-weight: 500;
+      line-height: normal;
+    `,
+    body5: css`
+      font-family: Pretendard;
+      font-size: 12px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 18px; /* 150% */
+    `,
   },
 };
 
