20b4677ae4dbb57c1828c540f5da1aa418ced660
namdaeun <nde40345@gmail.com>
2024-01-07 20:07:41 +0900

    fix: 테스트코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
 import styled from '@emotion/styled';
 
-type HeaderProps = {
-  type: boolean;
-};
-
 const App = () => {
-  //test
   return (
     <>
-      <Header type={true}>마일 웨비 화이팅</Header>
+      <Header>마일 웨비 화이팅</Header>
     </>
   );
 };
 
 export default App;
 
-const Header = styled.h1<HeaderProps>`
-  font-size: small;
-  color: ${({ theme }) => theme.colors.__main_point_violet};
-`;
+const Header = styled.h1``;
