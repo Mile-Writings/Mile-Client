ccc454ce9718b38d493e6d2c9465abb162575346
namdaeun <nde40345@gmail.com>
2024-01-07 18:34:16 +0900

    feat: GlobalStyle 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
-import { ThemeProvider } from '@emotion/react';
+import { Global, ThemeProvider } from '@emotion/react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
+import reset from './styles/GlobalStyle.tsx';
 import { theme } from './styles/theme.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Global styles={reset} />
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
