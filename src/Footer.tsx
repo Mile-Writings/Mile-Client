44ace2078a9a5ab25dd5245b4bd3ec6434ab0ff5
Moon Dahyun <dahyun723@ajou.ac.kr>
2024-01-09 02:07:38 +0900

    chore/#34 : theme 코딩 컨벤션 맞추기

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,13 +1,20 @@
 import styled from '@emotion/styled';
 
-import { FooterLogoSvg } from './assets/svgs';
+import { FooterLogoSvg, FooterInstaSvg, FooterKakaoSvg, FooterMailSvg } from './assets/svgs';
 
 const Footer = () => {
   return (
     <Wrapper>
-      footer
       <Layout>
         <FooterLogoSvg />
+        <MileText>Make it look easy, 글쓰기를 쉽고 편안하게</MileText>
+      </Layout>
+      <Layout>
+        <IconContainer>
+          <FooterMailSvg />
+          <FooterKakaoSvg />
+          <FooterInstaSvg />
+        </IconContainer>
       </Layout>
     </Wrapper>
   );
@@ -18,16 +25,30 @@ export default Footer;
 const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
-  width: 103.4rem;
-  height: 8.6rem;
+  width: 136.7rem;
+  height: 24.6rem;
   padding: 8rem 16.5rem;
 
   border: 1px solid red;
 `;
 
 const Layout = styled.div`
+  width: 24.5rem;
+  height: 8.09rem;
+`;
+
+const MileText = styled.div`
   width: 24.3rem;
   height: 8.09rem;
 
-  border: 1px solid yellow;
+  ${({ theme }) => theme.colors.gray80}
+  ${({ theme }) => theme.fonts.body6}
+`;
+
+const IconContainer = styled.div`
+  display: flex;
+  gap: 1.6rem;
+  justify-content: flex-end;
+  width: 24.3rem;
+  height: 8.09rem;
 `;
--- src/assets/svgs/index.tsx
+++ src/assets/svgs/index.tsx
@@ -1,5 +1,5 @@
 export { default as Rect } from './Rectangle 17.svg?react';
-export { default as FooterInstSvg } from './footerInstaSvg.svg?react';
+export { default as FooterInstaSvg } from './footerInstaSvg.svg?react';
 export { default as FooterKakaoSvg } from './footerKakaoSvg.svg?react';
 export { default as FooterMailSvg } from './footerMailSvg.svg?react';
 export { default as FooterLogoSvg } from './footerLogoSvg.svg?react';
