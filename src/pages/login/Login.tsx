0da926a9bff4fbe1bb50917af048dfdcffc28eb9
ljh0608 <m98m@catholic.ac.kr>
2024-01-09 16:13:52 +0900

    style: kakao login UI

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,5 +1,70 @@
+import styled from '@emotion/styled';
+
 const Login = () => {
-  return <div>Login</div>;
+  const handleKakaoLogin = () => {
+    alert('kakao login 준비중입니다.');
+  };
+  return (
+    <LoginWrapper>
+      <LoginContainer>
+        <LoginTextBox>
+          <HeadText>마일 시작하기</HeadText>
+          <SubText>Make it look easy 글쓰기를 쉽고 편안하게</SubText>
+        </LoginTextBox>
+        <LoginImageTemp />
+        <KakaoLoginBtn onClick={handleKakaoLogin} />
+      </LoginContainer>
+    </LoginWrapper>
+  );
 };
 
 export default Login;
+
+const LoginWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+
+  padding-top: 13.2rem;
+`;
+
+const LoginContainer = styled.div`
+  padding: 4rem 0;
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+  gap: 2.4rem;
+`;
+
+const LoginTextBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.6rem;
+  width: 30.9rem;
+  height: 7.4rem;
+`;
+
+const HeadText = styled.h1`
+  color: ${({ theme }) => theme.colors.black};
+  ${({ theme }) => theme.fonts.title2};
+`;
+
+const SubText = styled.p`
+  color: ${({ theme }) => theme.colors.gray90};
+  ${({ theme }) => theme.fonts.subtitle4};
+`;
+
+const LoginImageTemp = styled.div`
+  background-color: pink;
+  width: 21rem;
+  height: 14rem;
+`;
+
+const KakaoLoginBtn = styled.button`
+  width: 30rem;
+  height: 4.5rem;
+  background-image: url('src/assets/images/kakao_login_medium_wide.png');
+`;
