01f5ab2eaeaa51ee1104188627da4cc9c7113bd6
Moon Dahyun <dahyun723@ajou.ac.kr>
2024-01-09 15:00:41 +0900

    fix/#34 : spacing 코딩 컨벤션에 맞게 수정

--- src/components/commons/Spacing.tsx
+++ src/components/commons/Spacing.tsx
@@ -1,17 +1,16 @@
-import { FC } from 'react';
-
 import styled from '@emotion/styled';
 
-interface SpacingProps {
-  marginBottom?: string;
+interface SpacingPropTypes {
+  marginBottom: string;
 }
 
-const Spacing: FC<SpacingProps> = ({ marginBottom }) => {
-  return <SpacingBox marginBottom={marginBottom} />;
+const Spacing = (props: SpacingPropTypes) => {
+  const { marginBottom } = props;
+  return <SpacingBox $marginBottom={marginBottom} />;
 };
 
 export default Spacing;
 
-const SpacingBox = styled.div<SpacingProps>`
-  margin-bottom: ${(props) => props.marginBottom || '0'};
+const SpacingBox = styled.div<{ $marginBottom: string }>`
+  margin-bottom: ${(props) => props.$marginBottom || '0'};
 `;
