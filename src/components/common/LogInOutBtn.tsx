007b2bde201e9aace62c5bc83f158a0edf325c77
Moon Dahyun <dahyun723@ajou.ac.kr>
2024-01-09 16:12:52 +0900

    feat/#39 : LogInOutBtn 스타일링

--- src/components/common/LogInOutBtn.tsx
+++ src/components/common/LogInOutBtn.tsx
@@ -19,6 +19,16 @@ const LogInOutWrapper = styled.div`
   height: 4rem;
   padding: 1rem 1.6rem;
 
+  color: ${({ theme }) => theme.colors.gray70};
+
   border: 1px solid ${theme.colors.gray70};
   border-radius: 8px;
+
+  :hover {
+    color: ${({ theme }) => theme.colors.mainViolet};
+
+    border: 1px solid ${theme.colors.mainViolet};
+  }
+
+  ${({ theme }) => theme.fonts.button3}
 `;
