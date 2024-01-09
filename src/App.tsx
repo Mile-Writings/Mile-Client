3d9b03eb76473c1607f0e7f4d1f20a1dc2f63cdb
ljh0608 <m98m@catholic.ac.kr>
2024-01-09 16:13:25 +0900

    feat: desktop view 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
+import styled from '@emotion/styled';
+
 import Router from './Router';
 
 const App = () => {
   return (
-    <>
-      <Router />
-    </>
+    <DesktopWrapper>
+      <DesktopContainer>
+        <Router />
+      </DesktopContainer>
+    </DesktopWrapper>
   );
 };
 
 export default App;
+
+const DesktopWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+  width: 100%;
+`;
+
+const DesktopContainer = styled.div`
+  width: 136.8rem;
+`;
