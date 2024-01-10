cc1e4bed29d9888f655b67eec5f550f63c5d3b55
namdaeun <nde40345@gmail.com>
2024-01-11 07:24:32 +0900

    feat: 이미지 배치

--- src/pages/main/components/GroupContent.tsx
+++ src/pages/main/components/GroupContent.tsx
@@ -9,25 +9,25 @@ const GroupContent = ({ topic, maintext, subtext, image, isLast }: groupContenty
   };
 
   return (
-    <TextContainer>
-      <Topic>{topic}</Topic>
-      <MainText>{maintext}</MainText>
-      <Spacing marginBottom="3.2" />
-      <ImageWithTextBox>
+    <ContentLayout>
+      <TextContainer>
+        <Topic>{topic}</Topic>
+        <MainText>{maintext}</MainText>
+        <Spacing marginBottom="3.2" />
         <SubText isImage={hasImage()}>{subtext}</SubText>
-        {image !== null ? <Image src={image} isLast={isLast} alt="group-content-image" /> : <></>}
-        {isLast === true ? <Curious /> : <></>}
-      </ImageWithTextBox>
-    </TextContainer>
+      </TextContainer>
+      {image !== null ? <Image src={image} isLast={isLast} alt="group-content-image" /> : <></>}
+      {isLast === true ? <Curious /> : <></>}
+    </ContentLayout>
   );
 };
 
 export default GroupContent;
 
-const TextContainer = styled.div`
+const ContentLayout = styled.div`
   display: flex;
-  flex-direction: column;
   padding: 3.6rem;
+  gap: 3.6rem;
 `;
 
 const Topic = styled.div`
@@ -39,7 +39,10 @@ const MainText = styled.div`
   ${({ theme }) => theme.fonts.title10};
 `;
 
-const ImageWithTextBox = styled.div``;
+const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 const SubText = styled.div<{ isImage: boolean }>`
   width: ${({ isImage }) => (isImage ? '59.8rem' : '85.8rem')};
@@ -53,9 +56,9 @@ const SubText = styled.div<{ isImage: boolean }>`
 const Image = styled.img<{ isLast: boolean }>`
   width: ${({ isLast }) => (isLast ? '16.8rem' : '22.4rem')};
   height: 16.8rem;
-  position: absolute;
-  top: ${({ isLast }) => (isLast ? '5rem' : '5rem')};
-  left: ${({ isLast }) => (isLast ? '50rem' : '66.5rem')};
+  /* position: absolute; */
+  /* top: ${({ isLast }) => (isLast ? '5rem' : '5rem')};
+  left: ${({ isLast }) => (isLast ? '50rem' : '66.5rem')}; */
   z-index: 1;
 `;
 
