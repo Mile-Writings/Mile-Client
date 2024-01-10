f04d403468d6980266474003c35b958229466098
ljh0608 <m98m@catholic.ac.kr>
2024-01-11 03:46:05 +0900

    chore: svg 추가

--- src/assets/svgs/index.tsx
+++ src/assets/svgs/index.tsx
@@ -1,8 +1,10 @@
 export { default as Rect } from './Rectangle 17.svg?react';
-export { default as HeaderLogoSvg } from './headerLogoSvg.svg?react';
+export { default as DefaultProfileIc } from './defaultProfileIc.svg?react';
 export { default as FooterInstaSvg } from './footerInstaSvg.svg?react';
 export { default as FooterKakaoSvg } from './footerKakaoSvg.svg?react';
-export { default as FooterMailSvg } from './footerMailSvg.svg?react';
 export { default as FooterLogoSvg } from './footerLogoSvg.svg?react';
+export { default as FooterMailSvg } from './footerMailSvg.svg?react';
+export { default as HeaderLogoSvg } from './headerLogoSvg.svg?react';
 export { default as MakeGroupPlusBtn } from './mainMakeGroupBtnEnable.svg?react';
 export { default as MakeGroupPlusHoverBtn } from './mainMakeGroupBtnHover.svg?react';
+export { default as CheckboxIc } from './postDetailCheckIc.svg?react';
