88f50bf3aa39e058e12f8a7f0eb785820e6ad39b
namdaeun <nde40345@gmail.com>
2024-01-11 07:58:15 +0900

    feat: 더 궁금한 모임 컴포넌트

--- src/pages/main/components/Curious.tsx
+++ src/pages/main/components/Curious.tsx
@@ -1,14 +1,38 @@
-import styled from '@emotion/styled/macro';
+import styled from '@emotion/styled';
+
+import { MainGroupRoutingBtn } from '../../../assets/svgs';
+import Spacing from '../../../components/commons/Spacing';
 
 const Curious = () => {
   return (
-    <CuriousGroupText>
-      이 모임에 대해서
-      <br /> 더 궁금하신가요?
-    </CuriousGroupText>
+    <CuriousContentContainer>
+      <CuriousGroupText>
+        이 모임에 대해서
+        <br /> 더 궁금하신가요?
+      </CuriousGroupText>
+      <Spacing marginBottom="1.6" />
+      <GroupRoutingBtnBox>
+        <MainGroupRoutingBtn />
+      </GroupRoutingBtnBox>
+    </CuriousContentContainer>
   );
 };
 
 export default Curious;
 
-const CuriousGroupText = styled.div``;
+const CuriousContentContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+`;
+
+const CuriousGroupText = styled.p`
+  color: ${({ theme }) => theme.colors.mainViolet};
+  ${({ theme }) => theme.fonts.title8};
+`;
+
+const GroupRoutingBtnBox = styled.div`
+  & > MainGroupRoutingBtn:hover {
+    fill: ${({ theme }) => theme.colors.mileGreen};
+  }
+`;
