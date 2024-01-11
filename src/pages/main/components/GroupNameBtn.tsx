ae37bc0d46d5161324559aed10be28f3e5ebdaeb
namdaeun <nde40345@gmail.com>
2024-01-11 17:30:24 +0900

    feat: 버튼 스타일

--- src/pages/main/components/GroupNameBtn.tsx
+++ src/pages/main/components/GroupNameBtn.tsx
@@ -1,9 +1,33 @@
 import styled from '@emotion/styled';
 
+import { MainIcnArrowPurple } from '../../../assets/svgs';
+
 const GroupNameBtn = () => {
-  return <GroupNameButton />;
+  return (
+    <GroupNameButtonBox>
+      글 모임 이름
+      <MainIcnArrowPurple />
+    </GroupNameButtonBox>
+  );
 };
 
 export default GroupNameBtn;
 
-const GroupNameButton = styled.button``;
+const GroupNameButtonBox = styled.button`
+  display: flex;
+  gap: 0.8rem;
+  padding: 0.6rem 1rem;
+  align-items: center;
+  border-radius: 0.8rem;
+  /* margin-left: 21.8rem; */
+
+  border: 1px solid ${({ theme }) => theme.colors.mainViolet};
+  color: ${({ theme }) => theme.colors.mainViolet};
+  ${({ theme }) => theme.fonts.subtitle2};
+
+  &:hover > MainIcnArrowPurple {
+    path {
+      fill: ${({ theme }) => theme.colors.mileViolet};
+    }
+  }
+`;
