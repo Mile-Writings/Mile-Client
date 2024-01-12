13a6e11df27354bf4e6526edb01e149aa4cceea8
namdaeun <nde40345@gmail.com>
2024-01-13 05:15:30 +0900

    feat: 더미데이터 main에서 불러오기

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,5 +1,22 @@
+import styled from '@emotion/styled';
+
+import FaqDropdown from './components/FaqDropdown';
+import FaqTitle from './components/FaqTitle';
+import { FAQ_DATA } from './constants/faqData';
+
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <Wrapper>
+      <FaqTitle />
+      {FAQ_DATA.map(({ id, question, answer }) => (
+        <FaqDropdown key={id} id={id} question={question} answer={answer} />
+      ))}
+    </Wrapper>
+  );
 };
 
 export default Main;
+
+const Wrapper = styled.section`
+  background-color: ${({ theme }) => theme.colors.backGroundGray};
+`;
