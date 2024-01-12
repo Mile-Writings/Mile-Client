28c00a2f6779f3bdbbfa30ae79a094f079700083
namdaeun <nde40345@gmail.com>
2024-01-13 06:02:26 +0900

    init: font 추가

--- src/styles/emotion.d.ts
+++ src/styles/emotion.d.ts
@@ -42,12 +42,14 @@ declare module '@emotion/react' {
       title9: SerializedStyles;
       title10: SerializedStyles;
       title11: SerializedStyles;
+      title12: SerializedStyles;
       subtitle1: SerializedStyles;
       subtitle2: SerializedStyles;
       subtitle3: SerializedStyles;
       subtitle4: SerializedStyles;
       subtitle5: SerializedStyles;
       subtitle6: SerializedStyles;
+      subtitle7: SerializedStyles;
       body1: SerializedStyles;
       body2: SerializedStyles;
       body3: SerializedStyles;
