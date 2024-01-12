63a8a0ca61d180c174a9aad0ac8cafff23adddca
namdaeun <nde40345@gmail.com>
2024-01-12 22:39:51 +0900

    feat: 뷰에서 필요한 요소 설정

--- src/pages/main/components/FaqDropdown.tsx
+++ src/pages/main/components/FaqDropdown.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 
 import styled from '@emotion/styled';
 
+import { MainToggleArrowOpenedIc } from '../../../assets/svgs';
+
 const FaqDropdown = () => {
   const [dropDown, setDropdDown] = useState(false);
 
@@ -11,17 +13,48 @@ const FaqDropdown = () => {
   };
 
   return (
-    <DropdownWrapper>
-      <QuestionMarkText>Q.</QuestionMarkText>
-      <QuestionText>작자미상으로 글을 써도 다른 사람들이 알 수 있나요?</QuestionText>
-    </DropdownWrapper>
+    <FaqWrapper>
+      <TitleText>자주 묻는 질문</TitleText>
+      <DropdownContainer>
+        <QuestionTextBox>
+          <QuestionMarkText>Q.</QuestionMarkText>
+          <QuestionText>작자미상으로 글을 써도 다른 사람들이 알 수 있나요?</QuestionText>
+        </QuestionTextBox>
+        <MainToggleArrowOpenedIc onClick={handleDropDown} />
+      </DropdownContainer>
+    </FaqWrapper>
   );
 };
 
 export default FaqDropdown;
 
-const DropdownWrapper = styled.section``;
+const FaqWrapper = styled.section``;
 
-const QuestionMarkText = styled.p``;
+const TitleText = styled.p`
+  ${({ theme }) => theme.fonts.title3};
+`;
 
-const QuestionText = styled.p``;
+const DropdownContainer = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: fit-content;
+  padding: 2.4rem 2.8rem;
+
+  background-color: ${({ theme }) => theme.colors.gray30};
+`;
+
+const QuestionTextBox = styled.div`
+  display: flex;
+  gap: 0.8rem;
+  width: 85.3rem;
+`;
+
+const QuestionMarkText = styled.p`
+  color: ${({ theme }) => theme.colors.mainViolet};
+  ${({ theme }) => theme.fonts.title5};
+`;
+
+const QuestionText = styled.p`
+  ${({ theme }) => theme.fonts.title5};
+`;
