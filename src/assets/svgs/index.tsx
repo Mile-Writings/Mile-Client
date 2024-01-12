5686322264cc33c1d4fb140ab17e449b773c373d
namdaeun <nde40345@gmail.com>
2024-01-12 19:03:14 +0900

    feat: 필요한 svg 등록

--- src/assets/svgs/index.tsx
+++ src/assets/svgs/index.tsx
@@ -1,5 +1,3 @@
-
-
 export { default as HeaderLogoSvg } from './headerLogoSvg.svg?react';
 export { default as MainGroupRoutingBtn } from './mainGroupRoutingBtn.svg?react';
 export { default as MainGroupRoutingBtnHover } from './mainGroupRoutingBtnHover.svg?react';
@@ -10,15 +8,17 @@ export { default as GroupTabBtnBaseNextIc } from './groupTabBtnBaseNext.svg?reac
 
 export { default as FooterInstaIc } from './footerInstaSvg.svg?react';
 export { default as FooterKakaoIc } from './footerKakaoSvg.svg?react';
-export { default as FooterMailIc } from './footerMailSvg.svg?react';
 export { default as FooterLogoIc } from './footerLogoSvg.svg?react';
+export { default as FooterMailIc } from './footerMailSvg.svg?react';
 
+export { default as GroupCuriousProfileOpenIc } from './groupBestProfileIcnOpen.svg?react';
 export { default as GroupDateIc } from './groupDateIcn.svg?react';
 export { default as GroupLeaderIc } from './groupLeaderIcn.svg?react';
-export { default as GroupMemberIc } from './groupMemberIcn.svg?react';
-export { default as GroupCuriousProfileOpenIc } from './groupBestProfileIcnOpen.svg?react';
 export { default as GroupListProfileOpenIc } from './groupListProfileIcnOpen.svg?react';
+export { default as GroupMemberIc } from './groupMemberIcn.svg?react';
 
 export { default as HeaderLogoIc } from './headerLogoSvg.svg?react';
 export { default as MakeGroupPlusBtn } from './mainMakeGroupBtnEnable.svg?react';
 export { default as MakeGroupPlusHoverBtn } from './mainMakeGroupBtnHover.svg?react';
+export { default as MainTogglearrowClosedIc } from './mainTogglearrowClosedIc.svg?react';
+export { default as MainToggleArrowOpenedIc } from './mainTogglearrowOpenedIc.svg?react';
