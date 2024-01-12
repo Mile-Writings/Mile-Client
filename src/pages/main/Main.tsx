9c1f4f9d28e10b5745687b6d66e7958e42b0a2c1
namdaeun <nde40345@gmail.com>
2024-01-13 06:17:28 +0900

    feat: main에 OnBoarding 추가

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,16 +1,15 @@
 import styled from '@emotion/styled';
 
-import FaqDropdown from './components/FaqDropdown';
-import FaqTitle from './components/FaqTitle';
-import { FAQ_DATA } from './constants/faqData';
+import OnBoarding from './components/OnBoarding';
 
 const Main = () => {
   return (
     <Wrapper>
-      <FaqTitle />
+      <OnBoarding />
+      {/* <FaqTitle />
       {FAQ_DATA.map(({ id, question, answer }) => (
         <FaqDropdown key={id} id={id} question={question} answer={answer} />
-      ))}
+      ))} */}
     </Wrapper>
   );
 };
