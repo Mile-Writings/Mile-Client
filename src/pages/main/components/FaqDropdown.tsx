3ecf6742e92e292e2b18e52bc7f47cf2731333b6
namdaeun <nde40345@gmail.com>
2024-01-13 05:15:08 +0900

    feat: 더미데이터 불러오기

--- src/pages/main/components/FaqDropdown.tsx
+++ src/pages/main/components/FaqDropdown.tsx
@@ -3,9 +3,9 @@ import React, { useState } from 'react';
 import styled from '@emotion/styled';
 
 import { MainToggleArrowOpenedIc, MainTogglearrowClosedIc } from './../../../assets/svgs';
-import FaqTitle from './FaqTitle';
+import { faqDataPropTypes } from './../constants/faqData';
 
-const FaqDropdown = () => {
+const FaqDropdown = ({ id, question, answer }: faqDataPropTypes) => {
   const [dropDownOpened, setDropdDownOpened] = useState(false);
 
   const handleDropDown = (e: React.MouseEvent) => {
@@ -15,23 +15,16 @@ const FaqDropdown = () => {
 
   return (
     <FaqWrapper>
-      <FaqTitle />
       <DropdownLayout onClick={handleDropDown}>
         <FaqContainer>
-          <QuestionBox>
-            <QuestionMarkText>Q.</QuestionMarkText>
-            <QuestionText>작자미상과 필명은 무엇이 다른가요?</QuestionText>
-            {dropDownOpened ? <MainToggleArrowOpenedIc /> : <MainTogglearrowClosedIc />}
-          </QuestionBox>
-          {dropDownOpened && (
-            <AnswerTextBox>
-              작자미상은 글쓴이가 누군지 알 수 없는 철저한 익명의 상태입니다. 작자미상으로 글을
-              쓰면, 유저는 글쓴이의 어떠한 정보도 모른 채 글의 제목과 내용만을 볼 수 있습니다. 반면,
-              필명은 모임 안에서 사용되는 닉네임으로 글쓴이를 표현하는 기능입니다. 필명으로 글을
-              쓰면 유저들은 글쓴이가 설정한 필명 및 소개도 볼 수 있습니다. 자신을 완전히 드러내 글을
-              쓰고 싶다면, 필명을 실명으로 설정하는 방법도 있답니다.
-            </AnswerTextBox>
-          )}
+          <QuestionAnswerBox key={id}>
+            <Question>
+              <QuestionMarkText>Q.</QuestionMarkText>
+              <QuestionText>{question}</QuestionText>
+              {dropDownOpened ? <MainToggleArrowOpenedIc /> : <MainTogglearrowClosedIc />}
+            </Question>
+            {dropDownOpened && <AnswerTextBox>{answer}</AnswerTextBox>}
+          </QuestionAnswerBox>
         </FaqContainer>
       </DropdownLayout>
     </FaqWrapper>
@@ -40,7 +33,9 @@ const FaqDropdown = () => {
 
 export default FaqDropdown;
 
-const FaqWrapper = styled.section``;
+const FaqWrapper = styled.section`
+  margin-bottom: 1.4rem;
+`;
 
 const DropdownLayout = styled.section`
   display: flex;
@@ -57,7 +52,12 @@ const FaqContainer = styled.div`
   width: 85.3rem;
 `;
 
-const QuestionBox = styled.div`
+const QuestionAnswerBox = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Question = styled.div`
   display: flex;
   gap: 0.8rem;
   align-items: center;
