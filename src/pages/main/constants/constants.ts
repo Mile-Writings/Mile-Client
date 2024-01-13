46338d67932c527c9ea4070b5792f18d7737ec82
namdaeun <nde40345@gmail.com>
2024-01-14 01:32:15 +0900

    fix: 더미데이터 수정

--- src/pages/main/constants/constants.ts
+++ src/pages/main/constants/constants.ts
@@ -1,165 +1,115 @@
 import mainImgHotwriteLong from '../../../assets/images/mainImgHotwriteLong.png';
 
-// export interface MoimPost {
-//   topicName: string;
-//   imageUrl: string | null;
-//   postTitle: string;
-//   postContent: string;
-// }
-
-// export interface Moim {
-//   moimId: number;
-//   moimName: string;
-//   moimPosts: MoimPost[];
-// }
-
-// export interface Data {
-//   moim: Moim[];
-// }
-
-// const data: Data = {
-//   moim: [
-//     {
-//       moimId: 1,
-//       moimName: '마일',
-//       moimPosts: [
-//         {
-//           topicName: '주제',
-//           imageUrl: mainImgHotwriteLong,
-//           postTitle: '어쩌구',
-//           postContent: '저쩌구',
-//         },
-//         {
-//           topicName: '주제',
-//           imageUrl: mainImgHotwriteLong,
-//           postTitle: '어쩌구',
-//           postContent: '저쩌구',
-//         },
-//         {
-//           topicName: '주제',
-//           imageUrl: mainImgHotwriteLong,
-//           postTitle: '어쩌구',
-//           postContent: '저쩌구',
-//         },
-//         {
-//           topicName: '주제',
-//           imageUrl: mainImgHotwriteLong,
-//           postTitle: '어쩌구',
-//           postContent: '저쩌구',
-//         },
-//       ],
-//     },
-//     {
-//       moimId: 2,
-//       moimName: '마일',
-//       moimPosts: [
-//         {
-//           topicName: '주제',
-//           imageUrl: mainImgHotwriteLong,
-//           postTitle: '어쩌구',
-//           postContent: '저쩌구',
-//         },
-//         {
-//           topicName: '주제',
-//           imageUrl: mainImgHotwriteLong,
-//           postTitle: '어쩌구',
-//           postContent: '저쩌구',
-//         },
-//         {
-//           topicName: '주제',
-//           imageUrl: mainImgHotwriteLong,
-//           postTitle: '어쩌구',
-//           postContent: '저쩌구',
-//         },
-//         {
-//           topicName: '주제',
-//           imageUrl: mainImgHotwriteLong,
-//           postTitle: '어쩌구',
-//           postContent: '저쩌구',
-//         },
-//       ],
-//     },
-//     {
-//       moimId: 3,
-//       moimName: '마일',
-//       moimPosts: [
-//         {
-//           topicName: '주제',
-//           imageUrl: mainImgHotwriteLong,
-//           postTitle: '어쩌구',
-//           postContent: '저쩌구',
-//         },
-//         {
-//           topicName: '주제',
-//           imageUrl: mainImgHotwriteLong,
-//           postTitle: '어쩌구',
-//           postContent: '저쩌구',
-//         },
-//         {
-//           topicName: '주제',
-//           imageUrl: mainImgHotwriteLong,
-//           postTitle: '어쩌구',
-//           postContent: '저쩌구',
-//         },
-//         {
-//           topicName: '주제',
-//           imageUrl: mainImgHotwriteLong,
-//           postTitle: '어쩌구',
-//           postContent: '저쩌구',
-//         },
-//       ],
-//     },
-//   ],
-// };
-
-// export default data;
-
-export interface groupContentypes {
-  id: number;
-  topic: string;
-  maintext: string;
-  subtext: string;
-  image: string | null;
-  isLast: boolean;
+export interface MoimPostPropTypes {
+  topicName: string;
+  imageUrl: string | null;
+  postTitle: string;
+  postContent: string;
 }
 
-const GROUP_CONTENT: groupContentypes[] = [
-  {
-    id: 1,
-    topic: '글쓰기 주제가 입력될 공간',
-    maintext: '작성된 글의 제목이 들어갈 공간입니다.',
-    subtext:
-      '텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 ...',
-    image: mainImgHotwriteLong,
-    isLast: false,
-  },
-  {
-    id: 2,
-    topic: '글쓰기 주제가 입력될 공간',
-    maintext: '작성된 글의 제목이 들어갈 공간입니다.',
-    subtext:
-      '텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 ...',
-    image: null,
-    isLast: false,
-  },
-  {
-    id: 3,
-    topic: '글쓰기 주제가 입력될 공간',
-    maintext: '작성된 글의 제목이 들어갈 공간입니다.',
-    subtext:
-      '텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 ...',
-    image: null,
-    isLast: false,
-  },
-  {
-    id: 4,
-    topic: '글쓰기 주제가 입력될 공간',
-    maintext: '작성된 글의 제목이 들어갈 공간입니다.',
-    subtext:
-      '텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 공간입니다. 텍스트가 들어갈 ...',
-    image: mainImgHotwriteLong,
-    isLast: true,
-  },
-];
+export interface MoimPropTypes {
+  moimId: number;
+  moimName: string;
+  moimPosts: MoimPostPropTypes[];
+}
 
-export default GROUP_CONTENT;
+export interface DataPropTypes {
+  moim: MoimPropTypes[];
+}
+
+const CAROUSEL_DATA: DataPropTypes = {
+  moim: [
+    {
+      moimId: 1,
+      moimName: '마일',
+      moimPosts: [
+        {
+          topicName: '주제',
+          imageUrl: mainImgHotwriteLong,
+          postTitle: '어쩌구',
+          postContent: '저쩌구',
+        },
+        {
+          topicName: '주제',
+          imageUrl: mainImgHotwriteLong,
+          postTitle: '어쩌구',
+          postContent: '저쩌구',
+        },
+        {
+          topicName: '주제',
+          imageUrl: mainImgHotwriteLong,
+          postTitle: '어쩌구',
+          postContent: '저쩌구',
+        },
+        {
+          topicName: '주제',
+          imageUrl: mainImgHotwriteLong,
+          postTitle: '어쩌구',
+          postContent: '저쩌구',
+        },
+      ],
+    },
+    {
+      moimId: 2,
+      moimName: '마일',
+      moimPosts: [
+        {
+          topicName: '주제',
+          imageUrl: mainImgHotwriteLong,
+          postTitle: '어쩌구',
+          postContent: '저쩌구',
+        },
+        {
+          topicName: '주제',
+          imageUrl: mainImgHotwriteLong,
+          postTitle: '어쩌구',
+          postContent: '저쩌구',
+        },
+        {
+          topicName: '주제',
+          imageUrl: mainImgHotwriteLong,
+          postTitle: '어쩌구',
+          postContent: '저쩌구',
+        },
+        {
+          topicName: '주제',
+          imageUrl: mainImgHotwriteLong,
+          postTitle: '어쩌구',
+          postContent: '저쩌구',
+        },
+      ],
+    },
+    {
+      moimId: 3,
+      moimName: '마일',
+      moimPosts: [
+        {
+          topicName: '주제',
+          imageUrl: mainImgHotwriteLong,
+          postTitle: '어쩌구',
+          postContent: '저쩌구',
+        },
+        {
+          topicName: '주제',
+          imageUrl: mainImgHotwriteLong,
+          postTitle: '어쩌구',
+          postContent: '저쩌구',
+        },
+        {
+          topicName: '주제',
+          imageUrl: mainImgHotwriteLong,
+          postTitle: '어쩌구',
+          postContent: '저쩌구',
+        },
+        {
+          topicName: '주제',
+          imageUrl: mainImgHotwriteLong,
+          postTitle: '어쩌구',
+          postContent: '저쩌구',
+        },
+      ],
+    },
+  ],
+};
+
+export default CAROUSEL_DATA;
