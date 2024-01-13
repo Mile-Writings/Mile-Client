1a4c2c50328aad06993c0891e522f118f23774ac
Moon Dahyun <dahyun723@ajou.ac.kr>
2024-01-14 00:15:51 +0900

    feat/#82 : 쿼리 작성

--- src/pages/groupFeed/hooks/queries.ts
+++ src/pages/groupFeed/hooks/queries.ts
@@ -1,6 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 
-import { fetchGroupFeedAuth } from '../apis/fetchGroupFeed';
+import { fetchGroupFeedAuth, fetchGroupInfo } from '../apis/fetchGroupFeed';
 
 interface GroupFeedAuthQueryResult {
   isMember: boolean;
@@ -18,4 +18,29 @@ export const useGroupFeedAuth = (groupId: string): GroupFeedAuthQueryResult => {
   const isMember = data && data.data.isMember;
 
   return { isMember, isLoading, isError, error };
+};
+
+interface GroupInfoQueryResult {
+  groupInfoData: {
+    imageUrl: string;
+    moimName: string;
+    ownerName: string;
+    startDate: string;
+    writerCount: number;
+    description: string;
+  };
+  isLoading: boolean;
+  isError: boolean;
+  error: Error | null;
+}
+
+export const useGroupInfo = (groupId: string): GroupInfoQueryResult => {
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ['groupFeed_Info_moimId', groupId],
+    queryFn: () => fetchGroupInfo(groupId),
+  });
+
+  const groupInfoData = data && data.data;
+
+  return { groupInfoData, isLoading, isError, error };
 };
