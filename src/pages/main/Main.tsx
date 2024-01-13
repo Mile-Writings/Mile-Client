3026d2d0bc829cd8a4c2cd35db13879a08b66427
namdaeun <nde40345@gmail.com>
2024-01-13 15:37:19 +0900

    feat: Main에 GroupTitle 추가

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,19 +1,20 @@
 import styled from '@emotion/styled';
 
+import Footer from '../../components/commons/Footer';
 import Carousel from './components/Carousel';
 import FaqDropdown from './components/FaqDropdown';
 import FaqTitle from './components/FaqTitle';
+import GroupTitle from './components/GroupTitle';
 import Introduction from './components/Introduction';
 import Manual from './components/Manual';
 import Ruler from './components/Ruler';
 import { FAQ_DATA } from './constants/faqData';
-// import Summary from './components/Summary';
 
 const Main = () => {
   return (
     <MainPageWrapper>
       <CarouselComponentLayout>
-        {/* <Summary /> */}
+        <GroupTitle />
         <Carousel />
         <Carousel />
         <Carousel />
@@ -21,21 +22,16 @@ const Main = () => {
       <Ruler />
       <Introduction />
       <Manual />
-      <Wrapper>
-        <FaqTitle />
-        {FAQ_DATA.map(({ id, question, answer }) => (
-          <FaqDropdown key={id} id={id} question={question} answer={answer} />
-        ))}
-      </Wrapper>
+      <FaqTitle />
+      {FAQ_DATA.map(({ id, question, answer }) => (
+        <FaqDropdown key={id} id={id} question={question} answer={answer} />
+      ))}
+      <Footer />
     </MainPageWrapper>
   );
 };
 
 export default Main;
-
-const Wrapper = styled.section`
-  background-color: ${({ theme }) => theme.colors.backGroundGray};
-`;
 
 const MainPageWrapper = styled.div`
   background-color: ${({ theme }) => theme.colors.backGroundGray};
