23e8176c57c231acb05adc7d45a6ca4adf66a87c
namdaeun <nde40345@gmail.com>
2024-01-14 01:04:54 +0900

    feat: 인터페이스 모음 파일 생성

---
+++ src/pages/main/types/moimPost.ts
@@ -0,0 +1,16 @@
+export interface MoimPostPropTypes {
+  topicName: string;
+  imageUrl: string | null;
+  postTitle: string;
+  postContent: string;
+}
+
+export interface MoimPropTypes {
+  moimId: number;
+  moimName: string;
+  moimPosts: MoimPostPropTypes[];
+}
+
+export interface DataPropTypes {
+  moim: MoimPropTypes[];
+}
