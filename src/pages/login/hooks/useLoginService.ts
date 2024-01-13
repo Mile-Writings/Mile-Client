57ecbd1c9372a20bfc73aa7b15786f4d96aed6aa
ljh0608 <m98m@catholic.ac.kr>
2024-01-13 23:47:25 +0900

    chore: 주석 제거, 폴더명 변경

--- src/pages/login/hooks/useLoginService.ts
+++ src/pages/login/hooks/useLoginService.ts
@@ -1,7 +1,7 @@
 import { useMutation } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 
-import loginService from '../queries/loginService';
+import loginService from '../apis/loginService';
 import { LoginProps } from '../types/loginType';
 //추후 타입 필요할 지도 몰라서 넣어둠
 // interface LoginType {
@@ -14,7 +14,6 @@ import { LoginProps } from '../types/loginType';
 export const useLoginService = ({ code, socialType }: LoginProps) => {
   const navigate = useNavigate();
   return useMutation({
-    // { code, socialType }: LoginProps
     mutationFn: () => loginService(code, socialType),
     mutationKey: ['login'],
     onSuccess: (data) => {
