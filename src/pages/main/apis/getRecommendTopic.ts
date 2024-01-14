a548b7ac8dba337b259b46d749dfc27e8c4d0851
namdaeun <nde40345@gmail.com>
2024-01-14 17:42:43 +0900

    chore: 필요없는 콘솔 제거

--- src/pages/main/apis/getRecommendTopic.ts
+++ src/pages/main/apis/getRecommendTopic.ts
@@ -3,7 +3,6 @@ import { client } from '../../../utils/apis/axios';
 export const getRecommendTopic = async () => {
   try {
     const { data } = await client.get('/api/recommend/topic');
-    console.log(data.data);
     return { data: data.data };
   } catch (error) {
     console.error(error);
