c7895c69e19e422e9cc91b3d98e8d81bf91e1ee9
ljh0608 <m98m@catholic.ac.kr>
2024-01-14 22:01:02 +0900

    feat: PostDetail get API 연결

--- src/pages/postDetail/apis/fetchPostDetail.ts
+++ src/pages/postDetail/apis/fetchPostDetail.ts
@@ -1,8 +1,23 @@
 import { client } from '../../../utils/apis/axios';
 
+interface PostDetailRespnseTypes {
+  data: {
+    content: string;
+    createdAt: string;
+    imageUrl: string;
+    moimName: string;
+    title: string;
+    topic: string;
+    writerName: string;
+    writerInfo: string;
+  };
+  status: number;
+  message: string;
+}
+
 const fetchPostDetail = async (postId: string) => {
   try {
-    const data = await client.get(`/api/post/${postId}`);
+    const { data } = await client.get<PostDetailRespnseTypes>(`/api/post/${postId}`);
     console.log(data);
     return data;
   } catch (e) {
