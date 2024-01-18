213e28ddc33f8a158b54f13489bc85fcaafec82b
namdaeun <nde40345@gmail.com>
2024-01-18 19:25:20 +0900

    fix: 각 컴포넌트 내부에선 스켈레톤 호출 안함

--- src/pages/main/components/GroupContent.tsx
+++ src/pages/main/components/GroupContent.tsx
@@ -2,7 +2,6 @@ import styled from '@emotion/styled';
 import { useNavigate } from 'react-router-dom';
 
 import CuriousGroup from './CuriousGroup';
-import Skeleton from './Skeleton';
 
 import Spacing from './.././../../components/commons/Spacing';
 
@@ -34,7 +33,7 @@ const GroupContent = ({
 
   return (
     <ContentLayout onClick={handleOnClick}>
-      {imageUrl ? (
+      {imageUrl && (
         <>
           <TextContainer>
             <Topic>{topicName}</Topic>
@@ -45,16 +44,6 @@ const GroupContent = ({
             </SubText>
           </TextContainer>
           <Image src={imageUrl} isLast={isLast} alt="group-content-image" />
-        </>
-      ) : (
-        <>
-          <TextContainer>
-            <Skeleton width={59.8} height={1.7} unit={'rem'} color={'#dcdcdc'} rounded />
-            <Skeleton width={59.8} height={3.1} unit={'rem'} color={'#dcdcdc'} rounded />
-            <Spacing marginBottom="2" />
-            <Skeleton width={59.8} height={8.4} unit={'rem'} color={'#dcdcdc'} rounded />
-          </TextContainer>
-          <Skeleton width={59.8} height={1.7} unit={'rem'} color={'#dcdcdc'} circle rounded />
         </>
       )}
       {isLast && (
