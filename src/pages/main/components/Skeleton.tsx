50fef94c47d3b8bb3e92e572598d9a5cfa17052b
namdaeun <nde40345@gmail.com>
2024-01-18 15:16:38 +0900

    chore: 파일명 변경

---
+++ src/pages/main/components/Skeleton.tsx
@@ -0,0 +1,76 @@
+import { css, keyframes } from '@emotion/react';
+import styled from '@emotion/styled/macro';
+import React, { useMemo } from 'react';
+import 'react-loading-skeleton/dist/skeleton.css';
+
+interface skeletonPropTypes {
+  width: number;
+  height: number;
+  circle: boolean;
+  rounded: boolean;
+  count?: number;
+  unit: string; // 단위
+  animation: boolean;
+  color: string;
+  style?: React.CSSProperties;
+}
+
+const CarouselSkeleton = ({
+  width,
+  height,
+  circle,
+  rounded,
+  count,
+  unit,
+  animation,
+  color,
+  style,
+}: skeletonPropTypes) => {
+  const content = useMemo(() => [...Array({ length: count })].map(() => '-').join(''), [count]);
+  return (
+    <Base
+      style={style}
+      rounded={rounded}
+      circle={circle}
+      width={width}
+      height={height}
+      animation={animation}
+      unit={unit}
+      color={color}
+    >
+      <Content>{content}</Content>
+    </Base>
+  );
+};
+
+export default CarouselSkeleton;
+
+const pulseKeyframe = keyframes`
+  0% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0.4;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
+
+const pulseAnimation = css`
+  animation: ${pulseKeyframe} 1.5s ease-in-out infinite;
+`;
+
+const Base = styled.div<skeletonPropTypes>`
+  ${({ color }) => color && `background: ${color}`};
+  ${({ rounded }) => rounded && `border-radius: 8px`};
+  ${({ circle }) => circle && `border-radius: 50%`};
+  ${({ width, height }) => (width || height) && 'display: block'};
+  ${({ animation }) => animation && pulseAnimation};
+  width: ${({ width, unit }) => width && unit && `${width}${unit}`};
+  height: ${({ height, unit }) => height && unit && `${height}${unit}`};
+`;
+
+const Content = styled.span`
+  opacity: 0;
+`;
