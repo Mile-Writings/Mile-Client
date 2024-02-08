04156214167a38f8ee3f59a4689dd20b6dec2d64
namdaeun <nde40345@gmail.com>
2024-02-08 18:01:49 +0900

    feat: 캐러셀 api get 리액트 쿼리 적용

--- src/pages/main/hooks/queries.ts
+++ src/pages/main/hooks/queries.ts
@@ -1,4 +1,21 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { getGroupContent } from '../apis/getGroupContent';
+
 export const QUERY_KEY_MAIN = {
   getGroupContent: 'getGroupContent',
   getRecommendTopic: 'getRecommendTopic',
+};
+
+interface moimPropTypes {
+  moimId: string;
+}
+
+export const useGetGroupContent = ({ moimId }: moimPropTypes) => {
+  const { data } = useQuery({
+    queryKey: [QUERY_KEY_MAIN.getGroupContent, moimId],
+    queryFn: () => getGroupContent(),
+  });
+
+  return data;
 };
