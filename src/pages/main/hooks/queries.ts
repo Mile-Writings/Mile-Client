0a28b34e03ab781a9784a87909210f48615528af
namdaeun <nde40345@gmail.com>
2024-02-08 18:05:23 +0900

    feat: 글감 추천 api get 리액트 쿼리 적용

--- src/pages/main/hooks/queries.ts
+++ src/pages/main/hooks/queries.ts
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { getGroupContent } from '../apis/getGroupContent';
+import { getRecommendTopic } from '../apis/getRecommendTopic';
 
 export const QUERY_KEY_MAIN = {
   getGroupContent: 'getGroupContent',
@@ -15,6 +16,19 @@ export const useGetGroupContent = ({ moimId }: moimPropTypes) => {
   const { data } = useQuery({
     queryKey: [QUERY_KEY_MAIN.getGroupContent, moimId],
     queryFn: () => getGroupContent(),
+  });
+
+  return data;
+};
+
+interface recommendPropsTypes {
+  content: string;
+}
+
+export const useGetRecommendTopic = ({ content }: recommendPropsTypes) => {
+  const { data } = useQuery({
+    queryKey: [QUERY_KEY_MAIN.getRecommendTopic, content],
+    queryFn: () => getRecommendTopic(),
   });
 
   return data;
