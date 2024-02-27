0787a02ddffec6570f0e4e64c9101e6d1205f6c9
Moon Dahyun <dahyun723@ajou.ac.kr>
2024-02-27 22:00:36 +0900

    feat : 토큰 유무에 따라 헤더 분기

--- src/HeaderContainer.tsx
+++ src/HeaderContainer.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import { useNavigate, Outlet } from 'react-router-dom';
+
+import { GroupFeedHeader, LogOutHeader } from './pages/groupFeed/components/GroupFeedHeader';
 
 const HeaderContainer = () => {
-  return <div>sdds</div>;
+  const navigate = useNavigate();
+  const accessToken = localStorage.getItem('accessToken');
+  return (
+    <div>
+      {accessToken ? <GroupFeedHeader /> : <LogOutHeader onClick={() => navigate(`/login`)} />}
+      <Outlet />
+    </div>
+  );
 };
 
 export default HeaderContainer;
