8f94a27f99c3f2d994749263ea7ff96f2836e935
namdaeun <nde40345@gmail.com>
2024-03-03 01:19:05 +0900

    fix: isFetching 옵션 추가해서 타이틀 재배치 이슈 해결

--- src/pages/main/hooks/queries.ts
+++ src/pages/main/hooks/queries.ts
@@ -9,13 +9,13 @@ export const QUERY_KEY_MAIN = {
 };
 
 export const useGetGroupContent = (moimId: string) => {
-  const { data, isLoading } = useQuery({
+  const { data, isLoading, isFetching } = useQuery({
     queryKey: [QUERY_KEY_MAIN.getGroupContent, moimId],
     queryFn: () => getGroupContentApi(),
   });
   const groupLength = data?.length;
 
-  return { data, isLoading, groupLength };
+  return { data, isLoading, isFetching, groupLength };
 };
 
 export const useGetRecommendTopic = (content: string) => {
