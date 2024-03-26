c4f78205ee27085f131aad2e4f7a488589d03123
namdaeun <nde40345@gmail.com>
2024-03-26 17:35:07 +0900

    feat: 멤버 삭제 api useMutation

--- src/pages/admin/hooks/queries.ts
+++ src/pages/admin/hooks/queries.ts
@@ -1,11 +1,14 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
+import fetchDeleteMember from '../apis/fetchDeleteMember';
 import getMemberInfo from '../apis/getMemberInfo';
 
 export const QUERY_KEY_ADMIN = {
   useMemberInfo: 'getMemberInfo',
+  useDeleteMember: 'deleteMember',
 };
 
+// 멤버 정보 조회 get api
 export const useGetMemberInfo = (moimId: string) => {
   const { data, isLoading } = useQuery({
     queryKey: [QUERY_KEY_ADMIN.useMemberInfo],
@@ -14,4 +17,20 @@ export const useGetMemberInfo = (moimId: string) => {
   const totalMember = data?.writerNameCount;
 
   return { data, totalMember, isLoading };
+};
+
+// 멤버 삭제 api
+export const useDeleteMember = (writerNameId: string) => {
+  const queryClient = useQueryClient();
+  const data = useMutation({
+    mutationKey: [QUERY_KEY_ADMIN.useDeleteMember],
+    mutationFn: () => fetchDeleteMember(writerNameId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEY_ADMIN.useDeleteMember, writerNameId] });
+    },
+  });
+  const deleteMember = () => {
+    data.mutate();
+  };
+  return { deleteMember };
 };
