d54590a39bf1b09f64a1895cf166c719d1d33e13
Moon Dahyun <dahyun723@ajou.ac.kr>
2024-03-26 21:54:27 +0900

    feat/#273 : 글자수 count

--- src/pages/admin/AddEditTopicModal.tsx
+++ src/pages/admin/AddEditTopicModal.tsx
@@ -1,27 +1,61 @@
 import styled from '@emotion/styled';
-import React from 'react';
+import React, { useState } from 'react';
 
 import Spacing from '../../components/commons/Spacing';
 
 const AddEditTopicModal = () => {
+  const [topicName, setTopicName] = useState('');
+  const [topicTag, setTopicTag] = useState('');
+  const [topicDescription, setTopicDescription] = useState('');
+  const [topicDescriptionLength, setTopicDescriptionLength] = useState(0);
+  const limitLength = 90;
+
+  const handleTopicName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTopicName(e.target.value);
+  };
+
+  const handleTopicTag = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTopicTag(e.target.value);
+  };
+
+  const handleTopicDescription = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const newDescription = e.target.value;
+    setTopicDescription(newDescription);
+    setTopicDescriptionLength(newDescription.length);
+  };
   return (
     <ModalWrapper>
       <div>
         글감*
         <Spacing marginBottom="1.2" />
-        <TopicInput placeholder="함께 작성하고 싶은 글감을 입력해주세요. ex) 마음이 담긴 선물"></TopicInput>
+        <TopicInput
+          placeholder="함께 작성하고 싶은 글감을 입력해주세요. ex) 마음이 담긴 선물"
+          value={topicName}
+          onChange={handleTopicName}
+        ></TopicInput>
       </div>
       <div>
         글감 태그*
         <Spacing marginBottom="1.2" />
-        <TopicInput placeholder="위에 적은 글감을 한 단어로 요약해주세요. ex) 선물"></TopicInput>
+        <TopicInput
+          placeholder="위에 적은 글감을 한 단어로 요약해주세요. ex) 선물"
+          value={topicTag}
+          onChange={handleTopicTag}
+        ></TopicInput>
       </div>
       <div>
         글감 소개
         <Spacing marginBottom="1.2" />
-        <TopicDescriptionInput
-          placeholder={`글감에 대해 자유롭게 소개해주세요\nex) 마음이 담긴 선물을 주거나 받은 기억을 떠올려보세요.\n그 순간이 당신에게 어떤 의미로 남았는지 이야기해주세요.`}
-        ></TopicDescriptionInput>
+        <TextAreaWrapper>
+          <TopicDescriptionInput
+            placeholder={`글감에 대해 자유롭게 소개해주세요\nex) 마음이 담긴 선물을 주거나 받은 기억을 떠올려보세요.\n그 순간이 당신에게 어떤 의미로 남았는지 이야기해주세요.`}
+            onChange={handleTopicDescription}
+            maxLength={91}
+          ></TopicDescriptionInput>
+          <TextCount>
+            {topicDescriptionLength}/{limitLength}
+          </TextCount>
+        </TextAreaWrapper>
       </div>
       <SubmitForm>글감 수정하기</SubmitForm>
     </ModalWrapper>
@@ -61,24 +95,6 @@ const TopicInput = styled.input`
   }
 `;
 
-const TopicDescriptionInput = styled.textarea`
-  width: 55.2rem;
-  height: 11.8rem;
-  padding: 1rem 1.2rem;
-
-  color: ${({ theme }) => theme.colors.gray100};
-
-  ${({ theme }) => theme.fonts.button2}
-
-  background-color: ${({ theme }) => theme.colors.gray5};
-  border: 1px solid ${({ theme }) => theme.colors.gray50};
-  border-radius: 6px;
-
-  ::placeholder {
-    color: ${({ theme }) => theme.colors.gray50};
-  }
-`;
-
 const SubmitForm = styled.button`
   width: 55.2rem;
   height: 5.1rem;
@@ -89,4 +105,44 @@ const SubmitForm = styled.button`
   background-color: ${({ theme }) => theme.colors.mainViolet};
   ${({ theme }) => theme.fonts.button2}
   border-radius: 10px;
+`;
+
+const TopicDescriptionInput = styled.textarea`
+  width: 52.8rem;
+  height: 7.8rem;
+
+  color: ${({ theme }) => theme.colors.gray100};
+
+  background-color: ${({ theme }) => theme.colors.gray5};
+  ${({ theme }) => theme.fonts.button2}
+  border: none;
+  border-radius: 6px;
+
+  resize: none;
+
+  ::placeholder {
+    color: ${({ theme }) => theme.colors.gray50};
+  }
+
+  :focus {
+    outline: none;
+  }
+`;
+
+const TextAreaWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: end;
+  width: 55.2rem;
+  height: 11.8rem;
+  padding: 1rem 1.2rem;
+
+  background-color: ${({ theme }) => theme.colors.gray5};
+  border: 1px solid ${({ theme }) => theme.colors.gray50};
+  border-radius: 6px;
+`;
+
+const TextCount = styled.div`
+  ${({ theme }) => theme.fonts.button3}
+  color: ${({ theme }) => theme.colors.gray70};
 `;
