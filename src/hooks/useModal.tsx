ec742a1b0bde476aa54a56bbc2fb4a440dac9e83
namdaeun <nde40345@gmail.com>
2024-04-04 05:38:39 +0900

    feat: useModal에서 set함수까지 받아옴

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -3,13 +3,16 @@ import { useState } from 'react';
 const useModal = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const showModal = () => {
+    console.log(isModalOpen);
     setIsModalOpen(true);
   };
   const closeModal = () => {
+    console.log(isModalOpen);
     setIsModalOpen(false);
   };
   return {
     isModalOpen,
+    setIsModalOpen,
     showModal,
     closeModal,
   };
