e052920b5c44cc487ff25104ab1b841cc2edb25e
namdaeun <nde40345@gmail.com>
2024-04-06 00:16:55 +0900

    chore: 콘솔 제거

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -3,11 +3,9 @@ import { useState } from 'react';
 const useModal = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const showModal = () => {
-    console.log(isModalOpen);
     setIsModalOpen(true);
   };
   const closeModal = () => {
-    console.log(isModalOpen);
     setIsModalOpen(false);
   };
   return {
