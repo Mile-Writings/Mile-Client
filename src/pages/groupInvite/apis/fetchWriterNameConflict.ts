5582155ce26245fdae670b1522c1f9f63a96d90e
se0jinYoon <tjwls1999@gmail.com>
2024-04-10 13:27:50 +0900

    feat: 필명 중복 여부 쿼리, 훅 구현

---
+++ src/pages/groupInvite/apis/fetchWriterNameConflict.ts
@@ -0,0 +1,27 @@
+import { client } from '../../../utils/apis/axios';
+
+// 필명 중복 확인 GET
+interface FetchWriterNameConflictResponseTypes {
+  data: {
+    isConflict: boolean;
+  };
+  status: number;
+  message: string;
+}
+
+export const fetchWriterNameConflict = async (groupId: string, writerName: string) => {
+  try {
+    const accessToken = localStorage.getItem('accessToken');
+    const response = await client.get<FetchWriterNameConflictResponseTypes>(
+      `/api/moim/${groupId}/name/validation?writerName=${writerName}`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken},`,
+        },
+      },
+    );
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
--- src/pages/groupInvite/hooks/queries.ts
+++ src/pages/groupInvite/hooks/queries.ts
@@ -1,9 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { fetchGroupInfo } from '../apis/fetchGroupInfo';
+import { fetchWriterNameConflict } from '../apis/fetchWriterNameConflict';
 
 export const QUERY_KEY_GROUP_INVITE = {
   getGroupInfo: 'getGroupInfo',
+  getWriterNameConflict: 'getWriterNameConflict',
 };
 
 // 글 모임 정보 가져오기
@@ -20,4 +22,16 @@ export const useGetGroupInfo = (groupId: string) => {
   const description = data && data.data.description;
 
   return { moimTitle, imageUrl, leader, memberCount, description };
+};
+
+// 필명 중복 여부 확인
+export const useGetWriterNameConflict = (groupId: string, writerName: string) => {
+  const { data } = useQuery({
+    queryKey: [QUERY_KEY_GROUP_INVITE.getWriterNameConflict, groupId, writerName],
+    queryFn: () => fetchWriterNameConflict(groupId, writerName),
+  });
+
+  const isConflict = data && data.data.isConflict;
+
+  return { isConflict };
 };
