913ab95f7bd150e594a9d77a272fc71e9f943b4c
Moon Dahyun <dahyun723@ajou.ac.kr>
2024-04-13 21:23:50 +0900

    fix/#283: useAdminTopic에 groupId,pageNum 의존변수 추가

--- src/components/commons/Pagenation.tsx
+++ src/components/commons/Pagenation.tsx
@@ -1,13 +1,20 @@
 import styled from '@emotion/styled';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, Dispatch, SetStateAction } from 'react';
 
 import { ArrowLeftIc, ArrowRightIc } from '../../assets/svgs';
 import { slicePage } from '../../utils/countPage';
 import PageNumber from '../../utils/PageNumber';
 
-const Pagenation = ({ count, allocatedCount }: { count: number; allocatedCount: number }) => {
+interface pagenationPropTypes {
+  count: number;
+  allocatedCount: number;
+  setPageNum: Dispatch<SetStateAction<number>>;
+  pageNum: number;
+}
+
+const Pagenation = ({ count, allocatedCount, setPageNum, pageNum }: pagenationPropTypes) => {
   const [activeChunk, setActiveChunk] = useState(1);
-  const [activePage, setActivePage] = useState(1);
+  // const [activePages, setActivePages] = useState(1);
 
   const { resultArray, isExistNextPage, isExistPreviousPage } = slicePage(
     count,
@@ -16,17 +23,17 @@ const Pagenation = ({ count, allocatedCount }: { count: number; allocatedCount: 
   );
 
   useEffect(() => {
-    setActivePage(5 * (activeChunk - 1) + 1);
+    setPageNum(5 * (activeChunk - 1) + 1);
   }, [activeChunk]);
 
   return (
     <PageWrapper>
-      {activePage != 1 && (
+      {pageNum != 1 && (
         <ArrowLeftIc
           onClick={() =>
             isExistPreviousPage
               ? setActiveChunk(activeChunk - 1)
-              : setActivePage((prev) => {
+              : setPageNum((prev) => {
                   return prev - 1;
                 })
           }
@@ -36,8 +43,10 @@ const Pagenation = ({ count, allocatedCount }: { count: number; allocatedCount: 
       {resultArray.map((index) => (
         <PageNumber
           key={index + 1}
-          isActive={activePage === index}
-          onClick={() => setActivePage(index)}
+          isActive={pageNum === index}
+          onClick={() => {
+            setPageNum(index);
+          }}
         >
           {index}
         </PageNumber>
