fd675cdfcaf79220e7738ddef5e07a1aab86b625
namdaeun <nde40345@gmail.com>
2024-04-15 00:50:54 +0900

    chore: dev용 axios 코드 추가

--- src/utils/apis/axios.ts
+++ src/utils/apis/axios.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const baseUrl = import.meta.env.VITE_BASE_URL;
+const devBaseUrl = import.meta.env.VITE_DEV_URL;
 
 export const client = axios.create({
   baseURL: `${baseUrl}`,
@@ -11,3 +12,8 @@ export const client = axios.create({
 //   baseURL: `${baseUrl}`,
 //   headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
 // });
+
+export const devClient = axios.create({
+  baseURL: `${devBaseUrl}`,
+  headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
+});
