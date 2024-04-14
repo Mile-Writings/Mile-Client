ddaf0c014702321b97abe8f61009da301464ce4c
namdaeun <nde40345@gmail.com>
2024-04-15 00:51:36 +0900

    feat: 초대 링크 쿼리 코드

--- src/pages/admin/hooks/queries.ts
+++ src/pages/admin/hooks/queries.ts
@@ -1,6 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { fetchAdminTopic } from '../apis/fetchAdminData';
+import { fetchInvitationLink } from '../apis/fetchInvitationLink';
+
+export const QUERY_KEY_ADMIN = {
+  fetchInvitationLink: 'fetchInvitationLink',
+};
 
 export const useAdminTopic = () => {
   const { data, isLoading, isError, error } = useQuery({
@@ -12,4 +17,14 @@ export const useAdminTopic = () => {
   const adminTopicData = data && data.data;
 
   return { topicCount, adminTopicData, isLoading, isError, error };
+};
+
+export const useFetchInvitationLink = (groupId: string | undefined) => {
+  const { data } = useQuery({
+    queryKey: [QUERY_KEY_ADMIN.fetchInvitationLink],
+    queryFn: () => fetchInvitationLink(groupId || ''),
+  });
+  const invitationCode = data?.data;
+
+  return { invitationCode };
 };
