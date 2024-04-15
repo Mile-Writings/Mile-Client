7d738491efe61b5f7257c8e449bf936aad9d86a2
namdaeun <nde40345@gmail.com>
2024-04-16 02:00:40 +0900

    fix: 인터페이스 추가

--- src/pages/admin/apis/fetchInvitationLink.ts
+++ src/pages/admin/apis/fetchInvitationLink.ts
@@ -3,19 +3,28 @@ import { devClient } from './../../../utils/apis/axios';
 export interface InvitationPropTypes {
   status: number;
   message: string;
-  data: string;
+  data: invitationData;
+}
+
+export interface invitationData {
+  data: {
+    invitationCode: string;
+  };
 }
 
 export const fetchInvitationLink = async (groupId: string) => {
   try {
     const accessToken = localStorage.getItem('accessToken');
-    const data = await devClient.get<InvitationPropTypes>(`/api/moim/${groupId}/invitation-code`, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
+    const { data } = await devClient.get<InvitationPropTypes>(
+      `/api/moim/${groupId}/invitation-code`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
       },
-    });
-    console.log(accessToken);
-    return data.data;
+    );
+    console.log({ data }.data.data);
+    return { data: { data }.data.data };
   } catch {
     console.error();
   }
