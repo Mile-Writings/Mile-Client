8e2358d912bf44aae4b104d17a6e634f0f4e47af
se0jinYoon <tjwls1999@gmail.com>
2024-04-23 15:39:24 +0900

    design: 에디터 페이지 디폴트 이미지 변경

--- src/pages/postPage/constants/editorDefaultImg.ts
+++ src/pages/postPage/constants/editorDefaultImg.ts
@@ -1,2 +1,2 @@
 export const EDITOR_DEFAULT_IMG =
-  'https://mile-s3.s3.ap-northeast-2.amazonaws.com/post/KakaoTalk_Photo_2024-01-14-15-52-49.png';
+  'https://mile-s3.s3.ap-northeast-2.amazonaws.com/test/groupMile.png';
