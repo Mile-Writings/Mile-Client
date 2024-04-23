f26cda7a21e652eb7b6442c0f7cf8e588b3029c0
namdaeun <nde40345@gmail.com>
2024-04-23 23:38:56 +0900

    chore: baseUrl 변경

--- src/utils/apis/axios.ts
+++ src/utils/apis/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = import.meta.env.VITE_BASE_URL;
+const baseUrl = import.meta.env.VITE_DEV_URL;
 
 export const client = axios.create({
   baseURL: `${baseUrl}`,
