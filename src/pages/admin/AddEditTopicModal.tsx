afba5c73ffc6cebd28ead361ce448ab7763fa29c
Moon Dahyun <dahyun723@ajou.ac.kr>
2024-05-21 16:32:52 +0900

    feat/#325 : 글감 생성 모달 글자수 제한 추가

--- src/pages/admin/AddEditTopicModal.tsx
+++ src/pages/admin/AddEditTopicModal.tsx
@@ -36,7 +36,6 @@ const AddEditTopicModal = ({
   const [topicNameError, setTopicNameError] = useState(false);
   const [topicTagError, setTopicTagError] = useState(false);
   const [topicDescriptionError, setTopicDescriptionError] = useState(false);
-  const limitLength = 90;
 
   const { groupId } = useParams();
 
@@ -45,7 +44,7 @@ const AddEditTopicModal = ({
 
   const handleTopicNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTopic(e.target.value);
-    setTopicNameError(false);
+    setTopicNameError(false); //다시 입력할때의 초기화
   };
 
   const handleTopicTagChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -61,11 +60,15 @@ const AddEditTopicModal = ({
   };
 
   const handleSubmit = (topicStored: string) => {
-    if (topic.trim() === '' || topicTag.trim() === '' || topicDescription.trim() === '') {
-      setTopicNameError(topic.trim() === '');
-      setTopicTagError(topicTag.trim() === '');
-      setTopicDescriptionError(topicDescription.trim() === '');
-    } else {
+    const isTopicNameError = topic.trim() === '' || topic.length > 15;
+    const isTopicTagError = topicTag.trim() === '' || topicTag.length > 5;
+    const isTopicDescriptionError = topicDescription.trim() === '' || topicDescription.length > 90;
+
+    setTopicNameError(isTopicNameError);
+    setTopicTagError(isTopicTagError);
+    setTopicDescriptionError(isTopicDescriptionError);
+
+    if (!isTopicNameError && !isTopicTagError && !isTopicDescriptionError) {
       topicStored
         ? editMutateAdminTopic({ topic, topicTag, topicDescription, topicId })
         : postMutateAdminTopic({ topic, topicTag, topicDescription, groupId });
@@ -78,36 +81,42 @@ const AddEditTopicModal = ({
       <div>
         글감*
         <Spacing marginBottom="1.2" />
-        <TopicInput
-          placeholder="함께 작성하고 싶은 글감을 입력해주세요. ex) 마음이 담긴 선물"
-          value={topic}
-          onChange={handleTopicNameChange}
-          isError={topicNameError}
-        />
+        <InputWrapper isError={topic.length > 15 || topicNameError}>
+          <TopicInput
+            placeholder="함께 작성하고 싶은 글감을 입력해주세요. ex) 마음이 담긴 선물"
+            value={topic}
+            onChange={handleTopicNameChange}
+            maxLength={16}
+          />
+          <TextCount isError={topic.length > 15 || topicNameError}>{topic.length}/15</TextCount>
+        </InputWrapper>
       </div>
       <div>
         글감 태그*
         <Spacing marginBottom="1.2" />
-        <TopicInput
-          placeholder="위에 적은 글감을 한 단어로 요약해주세요. ex) 선물"
-          value={topicTag}
-          onChange={handleTopicTagChange}
-          isError={topicTagError}
-        />
+        <InputWrapper isError={topicTag.length > 5 || topicTagError}>
+          <TopicInput
+            placeholder="위에 적은 글감을 한 단어로 요약해주세요. ex) 선물"
+            value={topicTag}
+            onChange={handleTopicTagChange}
+            maxLength={6}
+          />
+          <TextCount isError={topicTag.length > 5 || topicTagError}>{topicTag.length}/5</TextCount>
+        </InputWrapper>
       </div>
       <div>
         글감 소개
         <Spacing marginBottom="1.2" />
-        <TextAreaWrapper>
+        <TextAreaWrapper isError={topicDescription.length > 90 || topicDescriptionError}>
           <TopicDescriptionInput
             placeholder={`글감에 대해 자유롭게 소개해주세요\nex) 마음이 담긴 선물을 주거나 받은 기억을 떠올려보세요.\n그 순간이 당신에게 어떤 의미로 남았는지 이야기해주세요.`}
             onChange={handleTopicDescriptionChange}
             value={topicDescription}
-            maxLength={limitLength}
+            maxLength={91}
             isError={topicDescriptionError}
           />
-          <TextCount>
-            {topicDescriptionLength}/{limitLength}
+          <TextCount isError={topicDescription.length > 90 || topicDescriptionError}>
+            {topicDescriptionLength}/90
           </TextCount>
         </TextAreaWrapper>
       </div>
@@ -137,18 +146,30 @@ const ModalWrapper = styled.div`
   border-radius: 8px;
 `;
 
-const TopicInput = styled.input<{ isError: boolean }>`
+const InputWrapper = styled.div<{ isError: boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
   width: 55.2rem;
   height: 3.9rem;
   padding: 1rem 1.2rem;
+
+  background-color: ${({ theme }) => theme.colors.gray5};
+  border: 1px solid
+    ${({ theme, isError }) => (isError ? theme.colors.mileRed : theme.colors.gray50)};
+  border-radius: 6px;
+`;
+
+const TopicInput = styled.input`
+  width: 46.4rem;
+  height: 100%;
 
   color: ${({ theme }) => theme.colors.gray100};
 
   ${({ theme }) => theme.fonts.button2}
 
   background-color: ${({ theme }) => theme.colors.gray5};
-  border: 1px solid ${({ theme, isError }) => (isError ? 'red' : theme.colors.gray50)};
-  border-radius: 6px;
+  border: none;
 
   ::placeholder {
     color: ${({ theme }) => theme.colors.gray50};
@@ -175,7 +196,8 @@ const TopicDescriptionInput = styled.textarea<{ isError: boolean }>`
   ${({ theme }) => theme.fonts.button2}
 
   background-color: ${({ theme }) => theme.colors.gray5};
-  border: 1px solid ${({ theme, isError }) => (isError ? 'red' : theme.colors.gray50)};
+  border: 1px solid
+    ${({ theme, isError }) => (isError ? theme.colors.mileRed : theme.colors.gray50)};
   border: none;
   border-radius: 6px;
 
@@ -190,7 +212,7 @@ const TopicDescriptionInput = styled.textarea<{ isError: boolean }>`
   }
 `;
 
-const TextAreaWrapper = styled.div`
+const TextAreaWrapper = styled.div<{ isError: boolean }>`
   display: flex;
   flex-direction: column;
   align-items: end;
@@ -199,11 +221,12 @@ const TextAreaWrapper = styled.div`
   padding: 1rem 1.2rem;
 
   background-color: ${({ theme }) => theme.colors.gray5};
-  border: 1px solid ${({ theme }) => theme.colors.gray50};
+  border: 1px solid
+    ${({ theme, isError }) => (isError ? theme.colors.mileRed : theme.colors.gray50)};
   border-radius: 6px;
 `;
 
-const TextCount = styled.div`
+const TextCount = styled.div<{ isError: boolean }>`
   ${({ theme }) => theme.fonts.button3}
-  color: ${({ theme }) => theme.colors.gray70};
+  color: ${({ theme, isError }) => (isError ? theme.colors.mileRed : theme.colors.gray70)};
 `;
