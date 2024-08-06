0c11a90d7b4b4c2eaea5addfbbdae4fe44cb6ba6
namdaeun <nde40345@gmail.com>
2024-08-06 22:24:09 +0900

    fix: 체크 박스 글자 영역 flex-end로 수정

--- src/pages/admin/components/EditGroupInfo.tsx
+++ src/pages/admin/components/EditGroupInfo.tsx
@@ -336,7 +336,7 @@ const GroupIsPublicWrapper = styled.div`
   display: flex;
   gap: 0.8rem;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-end;
   width: 8rem;
 `;
 const GroupisPublicLabel = styled.label`
@@ -352,6 +352,7 @@ const GroupisPublicLabel = styled.label`
 const GroupPublicDescContainer = styled.div`
   display: flex;
   gap: 0.8rem;
+  justify-content: flex-end;
   width: 8rem;
   height: 1.9rem;
 `;
