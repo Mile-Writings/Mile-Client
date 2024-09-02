e8f104fe93302b48a4f2b2bd442a71f301455fd8
se0jinYoon <tjwls1999@gmail.com>
2024-09-02 14:24:37 +0900

    feat: 페이지 이탈 함수 커스텀훅 구현

---
+++ src/hooks/useBlockPageExit.ts
@@ -0,0 +1,11 @@
+import { useBlocker } from 'react-router-dom';
+
+const useBlockPageExit = () => {
+  const blocker = useBlocker(
+    ({ currentLocation, nextLocation }) => currentLocation.pathname !== nextLocation.pathname,
+  );
+
+  return blocker;
+};
+
+export default useBlockPageExit;
