132b68821802d3f8f9453387bb4dbd94a9775ca5
se0jinYoon <tjwls1999@gmail.com>
2024-09-02 17:10:06 +0900

    fix: 페이지 이탈 hook 이탈 무시 조건부 추가

--- src/hooks/useBlockPageExit.ts
+++ src/hooks/useBlockPageExit.ts
@@ -3,25 +3,41 @@ import { useEffect, useState } from 'react';
 
 const useBlockPageExit = () => {
   const [isPageExitModalOpen, setIsPageExitModalOpen] = useState(false);
+  const [ignoreBlocker, setIgnoreBlocker] = useState(false);
 
-  const blocker = useBlocker(
-    ({ currentLocation, nextLocation }) => currentLocation.pathname !== nextLocation.pathname,
-  );
+  const blocker = useBlocker(({ currentLocation, nextLocation }) => {
+    // true 페이지 이동 허용
+    if (ignoreBlocker) {
+      return false;
+    }
 
+    // 그 외 경로 다르면 차단
+    return currentLocation.pathname !== nextLocation.pathname;
+  });
+
+  // 페이지 이탈 모달 열기
   useEffect(() => {
-    setIsPageExitModalOpen(blocker.state === 'blocked');
-  }, [blocker.state]);
+    !ignoreBlocker && setIsPageExitModalOpen(blocker.state === 'blocked');
+  }, [blocker.state, ignoreBlocker]);
 
+  // 페이지 이탈 모달 닫기
   const handleClosePageExitModal = () => {
     setIsPageExitModalOpen(false);
+    // block 초기화
     blocker?.reset && blocker.reset();
   };
 
+  // 페이지 나가기 (이탈 모달 확인 누를 경우)
   const handleExitPage = () => {
     blocker?.proceed && blocker.proceed();
   };
 
-  return { isPageExitModalOpen, handleClosePageExitModal, handleExitPage };
+  return {
+    isPageExitModalOpen,
+    handleClosePageExitModal,
+    handleExitPage,
+    setIgnoreBlocker,
+  };
 };
 
 export default useBlockPageExit;
