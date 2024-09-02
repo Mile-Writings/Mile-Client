a1b8d3717cc86924bea8828ec593801fddfdd7aa
se0jinYoon <tjwls1999@gmail.com>
2024-09-02 15:09:53 +0900

    fix: 페이지 이탈 모달 분리한 것 삭제, 모달 열고닫는 훅 useBlockPageExit에 추가

--- src/hooks/useBlockPageExit.ts
+++ src/hooks/useBlockPageExit.ts
@@ -1,11 +1,27 @@
 import { useBlocker } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 const useBlockPageExit = () => {
+  const [isPageExitModalOpen, setIsPageExitModalOpen] = useState(false);
+
   const blocker = useBlocker(
     ({ currentLocation, nextLocation }) => currentLocation.pathname !== nextLocation.pathname,
   );
 
-  return { blocker };
+  useEffect(() => {
+    setIsPageExitModalOpen(blocker.state === 'blocked');
+  }, [blocker.state]);
+
+  const handleClosePageExitModal = () => {
+    setIsPageExitModalOpen(false);
+    blocker?.reset && blocker.reset();
+  };
+
+  const handleExitPage = () => {
+    blocker?.proceed && blocker.proceed();
+  };
+
+  return { isPageExitModalOpen, handleClosePageExitModal, handleExitPage };
 };
 
 export default useBlockPageExit;
