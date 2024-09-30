17145688d326e1d11fc1b353d257f139dd5014b2
se0jinYoon <tjwls1999@gmail.com>
2024-09-30 22:28:36 +0900

    chore: 이메일 mailto 속성 추가

--- src/constants/footerLink.ts
+++ src/constants/footerLink.ts
@@ -1,5 +1,5 @@
 export const FOOTER_LINK = {
-  MAIL: 'unnamedwritings@gmail.com',
+  MAIL: 'mailto:unnamedwritings@gmail.com',
   INSTAGRAM: 'https://www.instagram.com/milewriting.official/',
   NOTION: 'https://milewriting.notion.site/fc4386f655b24be48dfa0946804c8e1e?pvs=4',
   WALLA: 'https://walla.my/survey/OjhPSWMQWbXmY8Nm9Ehv',
