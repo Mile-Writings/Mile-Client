492d5dbc4a64f33bea3e1d2a0c347a75ceb46434
ljh0608 <m98m@naver.com>
2024-10-17 16:08:28 +0900

    fix: put api return 데이터 제거

--- src/pages/postPage/apis/postDirectlyS3.ts
+++ src/pages/postPage/apis/postDirectlyS3.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const postDirectlyS3 = async (url: string, imageFile: File) => {
   try {
-    const data = await axios.put(`${url}`, imageFile, {
+    await axios.put(`${url}`, imageFile, {
       headers: {
         // 'Content-Type': 'application/octet-stream',
         'Content-Type': 'image/jpeg',
@@ -10,7 +10,6 @@ const postDirectlyS3 = async (url: string, imageFile: File) => {
         'Access-Control-Allow-Origin': '*',
       },
     });
-    return data;
   } catch (err) {
     console.log(err);
   }
