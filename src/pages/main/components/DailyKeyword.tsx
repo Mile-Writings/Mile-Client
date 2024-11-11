710d1a944f74c15a33aaf799d6589126eb513915
ljh0608 <m98m@naver.com>
2024-11-11 13:23:31 +0900

    design: DailyKeyword 반응형으로 변경

--- src/pages/main/components/DailyKeyword.tsx
+++ src/pages/main/components/DailyKeyword.tsx
@@ -1,39 +1,77 @@
 import styled from '@emotion/styled';
-
+import Responsive from '../../../components/commons/Responsive/Responsive';
+import { MOBILE_MEDIA_QUERY } from '../../../styles/mediaQuery';
 interface recommendPropsTypes {
   content: string | undefined;
 }
 const DailyKeyword = ({ content }: recommendPropsTypes) => {
   return (
-    <KeyWordWrapper>
-      <KeyWordLayout>
-        <KeywordHeaderContainer>
-          <KeywordContentBox>
-            <TodayKeyWord>오늘의 글감</TodayKeyWord>
-            <Pipe />
-            <KeyWord>{content}</KeyWord>
-          </KeywordContentBox>
-        </KeywordHeaderContainer>
-      </KeyWordLayout>
-    </KeyWordWrapper>
+    <>
+      <KeyWordWrapper>
+        <KeyWordLayout>
+          <Responsive only="desktop">
+            <KeywordHeaderContainer>
+              <KeywordContentBox>
+                <TodayKeyWord>오늘의 글감</TodayKeyWord>
+                <Pipe />
+                <KeyWord>{content}</KeyWord>
+              </KeywordContentBox>
+            </KeywordHeaderContainer>
+          </Responsive>
+          <Responsive only="mobile">
+            <KeywordHeaderContainer>
+              <MobileKeyWordHeader>오늘의 글감</MobileKeyWordHeader>
+              <MobileKeyWordText>{content}</MobileKeyWordText>
+            </KeywordHeaderContainer>
+          </Responsive>
+        </KeyWordLayout>
+      </KeyWordWrapper>
+    </>
   );
 };
 
 export default DailyKeyword;
 
+const MobileKeyWordText = styled.p`
+  color: ${({ theme }) => theme.colors.black};
+  font-weight: 600;
+  font-size: 18px;
+
+  /* mTitle 2 */
+  font-family: Pretendard, sans-serif;
+  font-style: normal;
+  line-height: 160%; /* 28.8px */
+`;
+
+const MobileKeyWordHeader = styled.header`
+  color: ${({ theme }) => theme.colors.mainViolet};
+  font-weight: 500;
+  font-size: 14px;
+
+  /* mSubtitle 2 */
+  font-family: Pretendard, sans-serif;
+  font-style: normal;
+  line-height: 140%; /* 19.6px */
+`;
 const KeyWordWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   width: 100%;
+  padding: 0 2rem;
 `;
 
 const KeyWordLayout = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  width: 93rem;
+  width: 100%;
+  max-width: 93rem;
   height: 8.4rem;
+
+  @media ${MOBILE_MEDIA_QUERY} {
+    max-width: 81rem;
+  }
 `;
 
 const KeywordHeaderContainer = styled.section`
@@ -43,7 +81,12 @@ const KeywordHeaderContainer = styled.section`
 
   background-color: ${({ theme }) => theme.colors.mileViolet};
   border-radius: 1rem;
+
   ${({ theme }) => theme.fonts.title11};
+  @media ${MOBILE_MEDIA_QUERY} {
+    gap: 0.8rem;
+    padding: 1.6rem 3.3rem;
+  }
 `;
 
 const KeywordContentBox = styled.div`
