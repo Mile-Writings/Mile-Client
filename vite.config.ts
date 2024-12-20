316d148d9ea4e2b85231fa6a5806125087dfd95f
ljh0608 <m98m@naver.com>
2024-12-20 17:56:30 +0900

    chore: 사용하지 않는 변수, 타입 주석처라ㅣ

--- vite.config.ts
+++ vite.config.ts
@@ -3,52 +3,51 @@ import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig, loadEnv, type PluginOption } from 'vite';
 import svgr from 'vite-plugin-svgr';
-import getAllpost from './src/utils/apis/getAllPost';
 
 // interface PostListWithGroup {
 //   [key: string]: string[]; // key는 문자열이고, 값은 문자열 배열
 // }
-type postListMoimMap = {
-  [key: string]: string[]; // key는 문자열, 값은 문자열 배열
-};
+// type postListMoimMap = {
+//   [key: string]: string[]; // key는 문자열, 값은 문자열 배열
+// };
 
-type postListMoimMap1 = {
-  key: string;
-  value: string; // key는 문자열,
-};
-interface PostListWithGroup {
-  postListMoimMap: postListMoimMap;
-}
-const generatePerformanceRoutes = async (url: string) => {
-  const staticRoutes = ['/']; // 정적 경로
-  const dynamicRoutes: string[] = []; // 동적 경로 저장 배열
+// type postListMoimMap1 = {
+//   key: string;
+//   value: string; // key는 문자열,
+// };
+// interface PostListWithGroup {
+//   postListMoimMap: postListMoimMap;
+// }
+// const generatePerformanceRoutes = async (url: string) => {
+//   const staticRoutes = ['/']; // 정적 경로
+//   const dynamicRoutes: string[] = []; // 동적 경로 저장 배열
 
-  const data: postListMoimMap1[] = await fetchAllPostWithGroup(url);
+//   const data: postListMoimMap1[] = await fetchAllPostWithGroup(url);
 
-  data.map((items: postListMoimMap1) => {
-    dynamicRoutes.push(`/detail/${items.key}/${items.value}`); // 정적 경로와 동적 경로를 결합하여 반환
-  });
+//   data.map((items: postListMoimMap1) => {
+//     dynamicRoutes.push(`/detail/${items.key}/${items.value}`); // 정적 경로와 동적 경로를 결합하여 반환
+//   });
 
-  // staticRoutes.concat(dynamicRoutes);
-  // console.log(staticRoutes);
-  return dynamicRoutes;
-};
+//   // staticRoutes.concat(dynamicRoutes);
+//   // console.log(staticRoutes);
+//   return dynamicRoutes;
+// };
 
-const fetchAllPostWithGroup = async (url: string) => {
-  const data = await getAllpost(url);
+// const fetchAllPostWithGroup = async (url: string) => {
+//   const data = await getAllpost(url);
 
-  const postListWithGroup: PostListWithGroup = data?.data?.data;
+//   const postListWithGroup: PostListWithGroup = data?.data?.data;
 
-  const result = Object.entries(postListWithGroup.postListMoimMap).flatMap(
-    ([key, values]: [string, string[]]) => values.map((value: string) => ({ key, value })),
-  );
+//   const result = Object.entries(postListWithGroup.postListMoimMap).flatMap(
+//     ([key, values]: [string, string[]]) => values.map((value: string) => ({ key, value })),
+//   );
 
-  return result;
-};
+//   return result;
+// };
 
 export default defineConfig(async ({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  const dynamicRoutes = await generatePerformanceRoutes(env.VITE_DEV_BASE_URL);
+  // const dynamicRoutes = await generatePerformanceRoutes(env.VITE_DEV_BASE_URL);
 
   return {
     // server: {
