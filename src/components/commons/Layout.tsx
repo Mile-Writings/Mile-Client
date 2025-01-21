5146b5d424a50ab1f36a8a4819422afe8e28c8c4
namdaeun <nde40345@gmail.com>
2025-01-21 17:08:09 +0900

    fix: RouteTracker 호출 위치 변경

--- src/components/commons/Layout.tsx
+++ src/components/commons/Layout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
+import RouteTracker from '../../routers/RouteTracker';
 import ScrollToTop from './ScrollToTop';
 
 const Layout = () => {
   return (
     <>
+      <RouteTracker />
       <ScrollToTop />
       <Outlet />
     </>
