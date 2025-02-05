b833b31a95e444303e0de20b1f37d80dec622283
ljh0608 <m98m@naver.com>
2025-02-05 14:57:47 +0900

    chore: 마일 이미지 default url 변경

--- src/constants/defaultImgUrl.ts
+++ src/constants/defaultImgUrl.ts
@@ -1,1 +1,1 @@
-export const DEFAULT_IMG_URL = 'https://mile-s3.s3.ap-northeast-2.amazonaws.com/test/groupMile.png';
+export const DEFAULT_IMG_URL = 'https://mile-new-s3.s3.ap-northeast-2.amazonaws.com/mile.png';
