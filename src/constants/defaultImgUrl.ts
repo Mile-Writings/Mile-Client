7fc8c8986a9711b5f162b5b3ac20e5c6b2351207
namdaeun <nde40345@gmail.com>
2026-01-04 19:43:15 +0900

    hotfix: change default img url

--- src/constants/defaultImgUrl.ts
+++ src/constants/defaultImgUrl.ts
@@ -1,1 +1,1 @@
-export const DEFAULT_IMG_URL = 'https://mile-new-s3.s3.ap-northeast-2.amazonaws.com/mile.png';
+export const DEFAULT_IMG_URL = 'https://2026mile.s3.ap-northeast-2.amazonaws.com/mile.png';
