52ecf2f9128aa315e8cbb2d9042af40ea77c6841
namdaeun <nde40345@gmail.com>
2026-01-05 12:02:49 +0900

    feat: editor default img 변경

--- src/pages/postPage/constants/editorDefaultImg.ts
+++ src/pages/postPage/constants/editorDefaultImg.ts
@@ -1,1 +1,1 @@
-export const EDITOR_DEFAULT_IMG = 'https://mile-new-s3.s3.ap-northeast-2.amazonaws.com/mile.png';
+export const EDITOR_DEFAULT_IMG = 'https://2026mile.s3.ap-northeast-2.amazonaws.com/mile.png';
